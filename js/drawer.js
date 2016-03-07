a1399d9deda92902803990d39494c892e9bc98fb
svkov <kovalev.svyatoslav@gmail.com>
2016-03-07 22:50:41 +0600

    first item

--- js/drawer.js
+++ js/drawer.js
@@ -1,30 +1,51 @@
 var main = document.getElementById("main");
 var ctx = main.getContext("2d");
-/*ctx.fillStyle = "#555";
-ctx.fillRect(100, 100, 200, 200);
-ctx.strokeStyle = "#FFF";
-ctx.beginPath();
-ctx.moveTo(50, 0);
-ctx.lineTo(100, 100);
-ctx.stroke();*/
+
 ctx.fillStyle = "#555";
 ctx.strokeStyle = "#fdfbfb";
-//ctx.fillRect(50, 50, 150, 150);
+
+//HEAD
 ctx.beginPath();
 ctx.moveTo(100, 0);
 ctx.quadraticCurveTo(50, 25, 100, 50);
 ctx.moveTo(100, 0);
 ctx.quadraticCurveTo(150, 25, 100, 50);
 ctx.stroke();
+
+//BODY
 ctx.beginPath();
 ctx.moveTo(100, 50);
 ctx.lineTo(100, 150);
+
+//LEFT LEG
 ctx.moveTo(100, 150);
 ctx.lineTo(75, 175);
+
+//RIGHT LEG
 ctx.moveTo(100, 150);
 ctx.lineTo(125, 175);
+
+//RIGHT HAND
 ctx.moveTo(100, 75);
 ctx.lineTo(125, 100);
+
+//LEFT HAND
 ctx.moveTo(100, 75);
 ctx.lineTo(75, 100);
 ctx.stroke();
+
+function drawSword() {
+    'use strict';
+    ctx.beginPath();
+    ctx.strokeStyle = '#afafaf';
+    ctx.lineWidth = 4;
+    ctx.moveTo(75, 100);
+    ctx.lineTo(65, 80);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.fillStyle = '#ff0000';
+    ctx.moveTo(55, 80);
+    ctx.quadraticCurveTo(100, 80, 40, 50);
+    ctx.fill();
+}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-var score = 0;
+var money = 0;
+var moneyPerClick = 1;
+var moneyPerSec = 0;
 
-function incrementScore() {
+var swordBuyed = false;
+
+function refreshMoney() {
     'use strict';
-    score += 1;
-    refreshScore();
+    document.getElementById('moneyCounter').innerHTML = money;
 }
 
-function refreshScore() {
+function incrementMoney() {
     'use strict';
-    document.getElementById('score').innerHTML = score;
+    money += moneyPerClick;
+    refreshMoney();
 }
 
+function buySword() {
+    'use strict';
+    var price = 50;
+    if (money < price) {
+        alert('You didn\'t have enough money');
+        return;
+    }
+    if (swordBuyed) {
+        alert('You already buy it!');
+        return;
+    }
+    money -= price;
+    swordBuyed = true;
+    moneyPerClick += 1;
+    drawSword();
+    refreshMoney();
+}
 
